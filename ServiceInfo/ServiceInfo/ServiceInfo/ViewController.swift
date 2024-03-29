39291a2c1fbaf1139972c63c3396875f90640a50
Apollonoff <apollonoff.arseniy@gmail.com>
2024-03-29 09:18:05 +0300

    Add project

---
+++ ServiceInfo/ServiceInfo/ServiceInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ServiceInfo
+//
+//  Created by Arseniy Apollonov on 29.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
